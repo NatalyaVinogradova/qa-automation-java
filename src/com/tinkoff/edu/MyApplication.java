d748d86024a6993b4a076bac9df749db6babcfb3
n.vinogradova <n.vinogradova@tinkoff.ru>
2021-08-09 16:40:49 +0300

    Задание 2. Изменены отступы на пробелы, все классы перенесены в пакеты и в классах добавлен автор.

---
+++ src/com/tinkoff/edu/MyApplication.java
@@ -0,0 +1,14 @@
+package com.tinkoff.edu;
+
+import com.tinkoff.edu.helpers.Printer;
+
+/**
+ * Created on 08.08.2021
+ *
+ * @author Natalya Vinogradova
+ */
+public class MyApplication {
+    public static void main(String[] args) {
+        Printer.print("Hello, World!");
+    }
+} 
