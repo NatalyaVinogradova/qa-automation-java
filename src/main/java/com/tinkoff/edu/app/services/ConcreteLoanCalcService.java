d855ac6258d8b2ce766dbf4c34a5696633b96a1d
n.vinogradova <n.vinogradova@tinkoff.ru>
2021-09-01 14:08:11 +0300

    Задание 8. Добавлены статики, оптимизированы импорты.

--- src/main/java/com/tinkoff/edu/app/services/ConcreteLoanCalcService.java
+++ src/main/java/com/tinkoff/edu/app/services/ConcreteLoanCalcService.java
@@ -1,9 +1,11 @@
 package com.tinkoff.edu.app.services;
 
-import com.tinkoff.edu.app.enums.ResponseType;
 import com.tinkoff.edu.app.models.LoanRequest;
 import com.tinkoff.edu.app.models.LoanResponse;
 import com.tinkoff.edu.app.repositories.LoanCalcRepository;
+
+import static com.tinkoff.edu.app.enums.ResponseType.APPROVED;
+import static com.tinkoff.edu.app.enums.ResponseType.DENIED;
 
 /**
  * Created on 13.08.2021
@@ -22,32 +24,32 @@ public class ConcreteLoanCalcService implements LoanCalcService {
 
     public LoanResponse createRequest(LoanRequest request) {
         if (request == null) {
-            return new LoanResponse(ResponseType.DENIED, -1, null);
+            return new LoanResponse(DENIED, -1, null);
         }
         if (request.getAmount() <= 0) {
-            return new LoanResponse(ResponseType.DENIED, -1, request);
+            return new LoanResponse(DENIED, -1, request);
         }
         if (request.getMonths() <= 0) {
-            return new LoanResponse(ResponseType.DENIED, -1, request);
+            return new LoanResponse(DENIED, -1, request);
         }
         LoanResponse response = repository.save(request);
         switch (request.getType()) {
             case PERSON:
                 if (request.getAmount() <= 10000 && request.getMonths() <= 12) {
-                    response.setType(ResponseType.APPROVED);
+                    response.setType(APPROVED);
                 } else {
-                    response.setType(ResponseType.DENIED);
+                    response.setType(DENIED);
                 }
                 break;
             case OOO:
                 if (request.getAmount() > 10000 && request.getMonths() < 12) {
-                    response.setType(ResponseType.APPROVED);
+                    response.setType(APPROVED);
                 } else if (request.getAmount() <= 10000 || request.getMonths() >= 12) {
-                    response.setType(ResponseType.DENIED);
+                    response.setType(DENIED);
                 }
                 break;
             case IP:
-                response.setType(ResponseType.DENIED);
+                response.setType(DENIED);
                 break;
         }
         return response;
--- src/test/java/com/tinkoff/edu/AppTest.java
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -1,8 +1,6 @@
 package com.tinkoff.edu;
 
 import com.tinkoff.edu.app.controllers.LoanCalcController;
-import com.tinkoff.edu.app.enums.LoanType;
-import com.tinkoff.edu.app.enums.ResponseType;
 import com.tinkoff.edu.app.models.LoanRequest;
 import com.tinkoff.edu.app.models.LoanResponse;
 import com.tinkoff.edu.app.repositories.VariableLoanCalcRepository;
@@ -10,6 +8,9 @@ import com.tinkoff.edu.app.services.ConcreteLoanCalcService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.tinkoff.edu.app.enums.LoanType.*;
+import static com.tinkoff.edu.app.enums.ResponseType.APPROVED;
+import static com.tinkoff.edu.app.enums.ResponseType.DENIED;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -23,7 +24,7 @@ public class AppTest {
 
     @BeforeEach
     public void init() {
-        request = new LoanRequest(LoanType.PERSON, 2500000, 60);
+        request = new LoanRequest(PERSON, 2500000, 60);
         controller = new LoanCalcController(new ConcreteLoanCalcService(new VariableLoanCalcRepository()));
     }
 
@@ -47,121 +48,121 @@ public class AppTest {
 
     @Test
     public void shouldGetErrorWhenApplyZeroOrNegativeAmountRequest() {
-        request = new LoanRequest(LoanType.PERSON, 0, 10);
+        request = new LoanRequest(PERSON, 0, 10);
         assertEquals(-1, controller.createRequest(request).getRequestId());
-        request = new LoanRequest(LoanType.PERSON, -10, 10);
+        request = new LoanRequest(PERSON, -10, 10);
         assertEquals(-1, controller.createRequest(request).getRequestId());
     }
 
     @Test
     public void shouldGetErrorWhenApplyZeroOrNegativeMonthsRequest() {
-        request = new LoanRequest(LoanType.PERSON, 1000, 0);
+        request = new LoanRequest(PERSON, 1000, 0);
         assertEquals(-1, controller.createRequest(request).getRequestId());
-        request = new LoanRequest(LoanType.PERSON, 1000, -10);
+        request = new LoanRequest(PERSON, 1000, -10);
         assertEquals(-1, controller.createRequest(request).getRequestId());
     }
 
     @Test
     public void shouldGetApprovedWhenPersonLess10000Less12() {
-        request = new LoanRequest(LoanType.PERSON, 9000, 11);
-        assertEquals(ResponseType.APPROVED, controller.createRequest(request).getType());
+        request = new LoanRequest(PERSON, 9000, 11);
+        assertEquals(APPROVED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetApprovedWhenPersonEquals10000Equals12() {
-        request = new LoanRequest(LoanType.PERSON, 10000, 12);
-        assertEquals(ResponseType.APPROVED, controller.createRequest(request).getType());
+        request = new LoanRequest(PERSON, 10000, 12);
+        assertEquals(APPROVED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetApprovedWhenPersonLess10000Equals12() {
-        request = new LoanRequest(LoanType.PERSON, 9000, 12);
-        assertEquals(ResponseType.APPROVED, controller.createRequest(request).getType());
+        request = new LoanRequest(PERSON, 9000, 12);
+        assertEquals(APPROVED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetApprovedWhenPersonEquals10000Less12() {
-        request = new LoanRequest(LoanType.PERSON, 10000, 9);
-        assertEquals(ResponseType.APPROVED, controller.createRequest(request).getType());
+        request = new LoanRequest(PERSON, 10000, 9);
+        assertEquals(APPROVED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenPersonMore10000Amount() {
-        request = new LoanRequest(LoanType.PERSON, 11000, 11);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(PERSON, 11000, 11);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenPersonMore12Months() {
-        request = new LoanRequest(LoanType.PERSON, 9000, 13);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(PERSON, 9000, 13);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetApprovedWhenOooMore10000Less12() {
-        request = new LoanRequest(LoanType.OOO, 15000, 11);
-        assertEquals(ResponseType.APPROVED, controller.createRequest(request).getType());
+        request = new LoanRequest(OOO, 15000, 11);
+        assertEquals(APPROVED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenOooLess10000More12() {
-        request = new LoanRequest(LoanType.OOO, 5000, 15);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(OOO, 5000, 15);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenOooLess10000Less12() {
-        request = new LoanRequest(LoanType.OOO, 5000, 6);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(OOO, 5000, 6);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenOooEquals10000More12() {
-        request = new LoanRequest(LoanType.OOO, 10000, 15);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(OOO, 10000, 15);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenOooEquals10000Less12() {
-        request = new LoanRequest(LoanType.OOO, 10000, 1);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(OOO, 10000, 1);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenOooLess10000Equals12() {
-        request = new LoanRequest(LoanType.OOO, 9000, 12);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(OOO, 9000, 12);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenOooMore10000More12() {
-        request = new LoanRequest(LoanType.OOO, 15000, 15);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(OOO, 15000, 15);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenOooMore10000Equals12() {
-        request = new LoanRequest(LoanType.OOO, 15000, 12);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(OOO, 15000, 12);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenOooEquals10000Equals12() {
-        request = new LoanRequest(LoanType.OOO, 10000, 12);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(OOO, 10000, 12);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 
     @Test
     public void shouldGetDeniedWhenIp() {
-        request = new LoanRequest(LoanType.IP, 10000, 12);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
-        request = new LoanRequest(LoanType.IP, 15000, 13);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
-        request = new LoanRequest(LoanType.IP, 2000, 5);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
-        request = new LoanRequest(LoanType.IP, 2000, 13);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
-        request = new LoanRequest(LoanType.IP, 15000, 5);
-        assertEquals(ResponseType.DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(IP, 10000, 12);
+        assertEquals(DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(IP, 15000, 13);
+        assertEquals(DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(IP, 2000, 5);
+        assertEquals(DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(IP, 2000, 13);
+        assertEquals(DENIED, controller.createRequest(request).getType());
+        request = new LoanRequest(IP, 15000, 5);
+        assertEquals(DENIED, controller.createRequest(request).getType());
     }
 }
